94c70324900ecd537b553baa2e4147134228e5be
Pitza <luishc23@icloud.com>
2025-01-21 16:40:31 -0600

    Add arrays.js file and enhance objects.js with comments
    Introduced a new `arrays.js` file demonstrating basic array usage and examples in JavaScript. Enhanced `objects.js` file with detailed explanatory comments for better clarity on object properties and operations.

---
+++ javascript_freeCodeCamp/project/arrays.js
@@ -0,0 +1,10 @@
+// Arrays in JavaScript are used to store a collection of items (elements) in a single variable.
+// These items can be of any type, including numbers, strings, objects, and even other arrays.
+// Arrays are zero-indexed, meaning the first element is accessed with index 0.
+
+//let exampleArray = [1, "hello", true, [2, 3], { key: "value" }];
+//console.log(exampleArray[0]); // Accessing the first element
+//console.log(exampleArray.length); // Getting the number of elements in the array
+
+let colors = ["red", "blue", "green", "yellow", "purple"];
+console.log(colors[1]);
--- javascript_freeCodeCamp/project/objects.js
+++ javascript_freeCodeCamp/project/objects.js
@@ -13,16 +13,22 @@
 //myObject.greet(); // Example of using a method within the object
 //console.log(myObject.interests[1]); // Accessing array property within the object
 
-let course ={
-    name: "Edgar Manuel Hernandez",
-    age: 24,
-    hours: 8,
-}
+let course = {
+    name: "Edgar Manuel Hernandez", // The 'name' property stores the string "Edgar Manuel Hernandez".
+    age: 24, // The 'age' property stores the number 24.
+    hours: 8, // The 'hours' property stores the number 8.
+};
 
+// Logging the entire 'course' object to the console.
 console.log(course);
+
+// Accessing and logging the value of the 'name' property of the 'course' object.
 console.log(course.name);
 
-course['name'] = 'James Smith';
+// Updating the value of the 'name' property in the 'course' object using bracket notation.
+course['name'] = 'James Smith'; // The 'name' property is now updated to "James Smith".
 
+// Logging the updated value of the 'name' property to the console.
 console.log(course.name);
+
 
