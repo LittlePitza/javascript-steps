b9390fa299799828d4c3891df0940ebd12a8ebf8
Pitza <luishc23@icloud.com>
2025-01-21 17:01:21 -0600

    Add operator functions and README file
    Implemented various arithmetic, logical, and bitwise operator functions in `operators-2.js` to support common operations like addition, subtraction, comparisons, and more. Additionally, added a placeholder `README.md` file for the Vanilla JavaScript Course directory.

---
+++ javascript_freeCodeCamp/project/operators-2.js
@@ -0,0 +1,103 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function modulus(a, b) {
+    return a % b;
+}
+
+function increment(a) {
+    return ++a;
+}
+
+function decrement(a) {
+    return --a;
+}
+
+function logicalAnd(a, b) {
+    return a && b;
+}
+
+function logicalOr(a, b) {
+    return a || b;
+}
+
+function logicalNot(a) {
+    return !a;
+}
+
+function bitwiseAnd(a, b) {
+    return a & b;
+}
+
+function bitwiseOr(a, b) {
+    return a | b;
+}
+
+function bitwiseXor(a, b) {
+    return a ^ b;
+}
+
+function bitwiseNot(a) {
+    return ~a;
+}
+
+function leftShift(a, b) {
+    return a << b;
+}
+
+function signedRightShift(a, b) {
+    return a >> b;
+}
+
+function unsignedRightShift(a, b) {
+    return a >>> b;
+}
+
+function greaterThan(a, b) {
+    return a > b;
+}
+
+function lessThan(a, b) {
+    return a < b;
+}
+
+function greaterThanOrEqual(a, b) {
+    return a >= b;
+}
+
+function lessThanOrEqual(a, b) {
+    return a <= b;
+}
+
+function equals(a, b) {
+    return a == b;
+}
+
+function strictEquals(a, b) {
+    return a === b;
+}
+
+function notEquals(a, b) {
+    return a != b;
+}
+
+function strictNotEquals(a, b) {
+    return a !== b;
+}
+
+function ternary(condition, a, b) {
+    return condition ? a : b;
+}
