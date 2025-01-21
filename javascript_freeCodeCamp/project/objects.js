d41b9dbebd465ed5bc758992a75e742ded487e10
Pitza <luishc23@icloud.com>
2025-01-21 16:35:12 -0600

    Add a basic JavaScript object example.
    Created a new file showcasing examples of defining, modifying, and accessing objects in JavaScript. Included examples of properties, methods, and array manipulation to explain object behavior clearly.

---
+++ javascript_freeCodeCamp/project/objects.js
@@ -0,0 +1,28 @@
+// Objects in JavaScript are collections of key-value pairs, where keys are strings or symbols.
+// Values can be any datatype, including other objects, functions, arrays, or primitive types.
+
+//const myObject = {
+//    name: "John",
+//    age: 30,
+//    greet: function() {
+//        console.log(`Hello, my name is ${this.name}!`);
+//    },
+//    interests: ["reading", "sports", "coding"]
+//};
+//
+//myObject.greet(); // Example of using a method within the object
+//console.log(myObject.interests[1]); // Accessing array property within the object
+
+let course ={
+    name: "Edgar Manuel Hernandez",
+    age: 24,
+    hours: 8,
+}
+
+console.log(course);
+console.log(course.name);
+
+course['name'] = 'James Smith';
+
+console.log(course.name);
+
