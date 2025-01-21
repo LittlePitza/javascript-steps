97126e2af4506062d9b61c1b06e8776f3641a595
Pitza <luishc23@icloud.com>
2025-01-21 16:44:25 -0600

    Add initial JavaScript functions file
    Created a new file `functions.js` with examples of basic functions, including commented-out placeholder functions and an active `sayHi` function that logs a greeting. This sets the foundation for further developments in the project.

---
+++ javascript_freeCodeCamp/project/functions.js
@@ -0,0 +1,19 @@
+// A function in JavaScript is a block of reusable code that performs a specific task. Functions are defined with the "function" keyword, can take parameters, and return a value.
+
+//function add(a, b) {
+//    return a + b;
+//}
+//
+//function greet(name) {
+//    return `Hello, ${name}!`;
+//}
+//
+//function multiply(x, y) {
+//    return x * y;
+//}
+
+function sayHi(name){
+    return console.log('Hi! ' + name);
+}
+
+sayHi('Luis');
